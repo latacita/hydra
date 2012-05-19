14b2197b335b6d5cea699c14f88cbe45e95ed239
pablo@lcc.uma.es <pablo@lcc.uma.es@c5724cf3-a313-91a5-481f-bcaaae864fe7>
2012-05-19 17:01:32 +0000

    Initial Version of the new brabch of Hydra, which has been called Hydracan.

---
+++ Hydracan/syntax.resource.constraint.ui/src-gen/syntax/resource/constraint/ui/ConstraintPositionCategory.java
@@ -0,0 +1,14 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ * 
+ */
+package syntax.resource.constraint.ui;
+
+/**
+ * An enumeration of all position categories.
+ */
+public enum ConstraintPositionCategory {
+	BRACKET, DEFINTION, PROXY;
+}
